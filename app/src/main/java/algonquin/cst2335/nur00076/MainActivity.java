43509e2504e62a5846802462904353fba346b4c7
guled <abc>
2022-09-28 13:38:05 -0400

    5th commit: Adding compound buttons

--- app/src/main/java/algonquin/cst2335/nur00076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/nur00076/MainActivity.java
@@ -3,12 +3,38 @@ package algonquin.cst2335.nur00076;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import algonquin.cst2335.nur00076.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        TextView mytext = variableBinding.textview;
+        EditText myedit = variableBinding.myedittext;
+
+        String editString = myedit.getText().toString();
+        mytext.setText( "Your edit text has: " + editString);
+
+        final Button btn = findViewById(R.id.mybutton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+
     }
+
 }
