4d036244a943fbd44e6ee515ebd8efed60e46340
guled <abc>
2022-11-15 19:22:46 -0500

    13th commit: Finished lab6

---
+++ app/src/main/java/algonquin/cst2335/nur00076/receive_message.java
@@ -0,0 +1,18 @@
+package algonquin.cst2335.nur00076;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class receive_message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
---
+++ app/src/main/java/data/ChatMessage.java
@@ -0,0 +1,29 @@
+package data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatMessage {
+    String message;
+    String timeSent;
+    boolean isSentButton;
+
+    public ChatMessage(String m, String t, boolean sent) {
+        message = m;
+        timeSent = t;
+        isSentButton = sent;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public boolean isSentButton() {
+        return isSentButton;
+    }
+}
